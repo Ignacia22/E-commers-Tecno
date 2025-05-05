a665a6494acd3eac277ed512648bc4094e91bc7b
Ignacia22 <mfernandezpolanco@gmail.com>
2025-05-05 17:17:54 -0400

    arreglo

--- next-proyect/src/helpers/getProducts.ts
+++ next-proyect/src/helpers/getProducts.ts
@@ -3,6 +3,6 @@ import { IProducts } from "@/Interface/IProducts"
 import axios from "axios"
 
 export const getProducts = async (): Promise<IProducts[]> => {
-    const fetch = await axios.get(`${process.env.API_URL}/products`)
+    const fetch = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products`)
     return fetch.data
 }
